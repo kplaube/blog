72f0e3e48fc8989f4731eae9afbc71cc00baaee7
Klaus Laube <klaus@corp.globo.com>
2013-04-02 10:22:24 -0300

    Updating settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,16 +19,33 @@ FEED_ALL_ATOM = 'feed/atom.xml'
 
 DEFAULT_PAGINATION = 10
 
-# Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+# Menu
+MENUITEMS = (
+    ('Blog', '/'),
+    ('Portfolio', '/portfolio/'),
+)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Blogroll
+LINKS = (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+    ('You can modify those links in your config file', '#'),
+)
+
+# Social
+SOCIAL = (
+    ('You can add links in your config file', '#'),
+    ('Another social link', '#'),
+)
+
+TWITTER_USER = 'kplaube'
+
+LICENSE = 'Creative Commons Attribution 3.0'
+LICENSE_URL = 'http://creativecommons.org/licenses/by/3.0/deed.pt_BR'
+SOURCE_CODE_URL = 'https://github.com/kplaube/klauslaube.com.br/'
 
 PLUGINS = (
     'pelican.plugins.assets',
+    'pelican.plugins.global_license',
 )
