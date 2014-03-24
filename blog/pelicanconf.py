5d86842ad2d071190379f4aee78a264fbf56e609
Klaus Laube <kplaube@gmail.com>
2014-03-24 14:30:45 -0300

    Configuring tag cloud plugin

--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -35,6 +35,7 @@ DEFAULT_PAGINATION = 10
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 TAG_URL = '/tag/{slug}.html'
+TAG_CLOUD_MAX_ITEMS = 30
 DISQUS_SITENAME = 'klauslaube'
 
 # Menu
