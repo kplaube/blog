2f67f07b21cbe85d5082fe30623dd62cea38c0a2
Klaus Laube <kplaube@gmail.com>
2017-10-11 18:21:36 -0300

    HTTPS, baby

--- terraform/droplet.tf
+++ terraform/droplet.tf
@@ -16,20 +16,28 @@ resource "digitalocean_droplet" "blog" {
 
     provisioner "remote-exec" {
         inline = [
+            "sudo add-apt-repository ppa:certbot/certbot -y",
             "sudo apt-get update",
-            "sudo apt-get -y install nginx"
+            "sudo apt-get -y install nginx python-certbot-nginx",
+            "sudo ufw allow OpenSSH",
+            "sudo ufw allow 80",
+            "sudo ufw allow 443",
+            "sudo ufw enable",
+            "sudo certbot certonly --standalone --preferred-challenges tls-sni -d klauslaube.com.br",
+            "sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048"
         ]
     }
 
     provisioner "file" {
-        source = "./conf/nginx.conf"
+        source = "./terraform/conf/nginx.conf"
         destination = "/etc/nginx/sites-available/blog"
     }
 
     provisioner "remote-exec" {
         inline = [
             "ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/",
-            "rm -f /etc/nginx/sites-enabled/default"
+            "rm -f /etc/nginx/sites-enabled/default",
+            "sudo service nginx restart"
         ]
     }
 }
